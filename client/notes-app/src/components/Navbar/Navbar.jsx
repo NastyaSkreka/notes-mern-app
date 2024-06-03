9afaecc6ca92424e2c82f8f4f760b12eb68b9c66
NastyaSkreka <skreka.nastya@gmail.com>
2024-06-03 15:26:17 +0300

    feat: add navbar profile card

--- client/notes-app/src/components/Navbar/Navbar.jsx
+++ client/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
+import ProfileInfo from "../Cards/ProfileInfo";
+import {useNavigate } from "react-router-dom"
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between px-5 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
--- client/notes-app/src/pages/Home/Home.jsx
+++ client/notes-app/src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Navbar/>
     )
 }
 
