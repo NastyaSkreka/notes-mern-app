c970d9f1cc9b66c1b67d10fd07577e704461a502
NastyaSkreka <skreka.nastya@gmail.com>
2024-06-03 15:57:27 +0300

    feat: add search bar component

--- client/notes-app/src/components/Navbar/Navbar.jsx
+++ client/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import {useNavigate } from "react-router-dom"
+import SearchBar from "../SearchBar/SearchBar";
+
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   const onLogout = () => {
     navigate("/login");
   };
 
+  const handleSearch = () => {
+
+  }
+  const onClearSearch = () => {
+    setSearchQuery("")
+  }
+
   return (
     <div className="bg-white flex items-center justify-between px-5 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+            setSearchQuery(target.value)
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
       <ProfileInfo onLogout={onLogout} />
     </div>
   );
