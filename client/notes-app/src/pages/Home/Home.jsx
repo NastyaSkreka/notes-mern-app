9eba00aea85fd50a5a359e4e4c115c30f7c2eab1
NastyaSkreka <skreka.nastya@gmail.com>
2024-06-04 11:00:17 +0300

    feat: add popup & tag input component

--- client/notes-app/src/pages/Home/Home.jsx
+++ client/notes-app/src/pages/Home/Home.jsx
@@ -1,30 +1,62 @@
+import { useState } from "react";
 import NoteCard from "../../components/Cards/NoteCard";
 import Navbar from "../../components/Navbar/Navbar";
-import { MdAdd } from "react-icons/md"
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
     <>
       <Navbar />
-
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-        <NoteCard
-          title="Meeting on 7th April"
-          date="3rd Apr 2024"
-          content="Meeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th April"
-          tags="#Meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-        />
+          <NoteCard
+            title="Meeting on 7th April"
+            date="3rd Apr 2024"
+            content="Meeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th AprilMeeting on 7th April"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
         </div>
       </div>
 
-      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-primary-600 absolute right-10 bottom-10" onClick={() => {}}>
-        <MdAdd className="text-[32px] text-white"/>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-primary-600 absolute right-10 bottom-10"
+        onClick={() =>
+          setOpenAddEditModal({ isShown: true, type: "add", data: null })
+        }
+      >
+        <MdAdd className="text-[32px] text-white" />
       </button>
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
+      </Modal>
     </>
   );
 };
